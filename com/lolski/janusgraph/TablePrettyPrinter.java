fd864986e3a690f3bc75e10ab596f0050824c27d
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-06-13 12:37:59 +0100

    Read janusgraph_ids. Add a code that adds a single vertex and retreives list of vertices for examining the write path

--- com/lolski/janusgraph/TablePrettyPrinter.java
+++ com/lolski/janusgraph/TablePrettyPrinter.java
@@ -4,6 +4,7 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
 import org.janusgraph.diskstorage.util.StaticArrayBuffer;
@@ -11,7 +12,11 @@ import org.janusgraph.graphdb.database.StandardJanusGraph;
 import org.janusgraph.graphdb.idmanagement.IDManager;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+@SuppressWarnings("Duplicates")
 public class TablePrettyPrinter {
     public static void main(String[] args) {
         String host = "localhost";
@@ -30,14 +35,25 @@ public class TablePrettyPrinter {
             IDManager idManager = graph.getIDManager();
             cluster = Cluster.builder().addContactPoint(host).build();
             session = cluster.connect(keyspace);
-
-            // print edgestore
-            ResultSet results = session.execute("select * from edgestore");
-            for (Row result : results.all()) {
+            graph.addVertex("henlo");
+            List<Vertex> v = graph.traversal().V().hasLabel("henlo").toList();
+            System.out.println("------ size=" + v.size() + ", id=" + v.get(0));
+            // print janusgraph_ids
+            ResultSet janusgraph_ids1 = session.execute("select * from janusgraph_ids");
+            for (Row result : janusgraph_ids1.all()) {
                 StaticArrayBuffer key = StaticArrayBuffer.of(result.getBytes("key"));
                 StaticArrayBuffer column1 = StaticArrayBuffer.of(result.getBytes("column1"));
                 StaticArrayBuffer value = StaticArrayBuffer.of(result.getBytes("value"));
-                System.out.println("key=[" + idManager.getKeyID(key) + "], column1=[" + column1 + "], value=[" + value + "]");
+                System.out.println("------ janusgraph_ids: key=[" + key + "], column1=[" + column1 + "], value=[" + value + "]");
+            }
+
+            // print edgestore
+            ResultSet edgestore1 = session.execute("select * from edgestore");
+            for (Row result : edgestore1.all()) {
+                StaticArrayBuffer key = StaticArrayBuffer.of(result.getBytes("key"));
+                StaticArrayBuffer column1 = StaticArrayBuffer.of(result.getBytes("column1"));
+                StaticArrayBuffer value = StaticArrayBuffer.of(result.getBytes("value"));
+                System.out.println("------ edgestore: key=[" + idManager.getKeyID(key) + "], column1=[" + column1 + "], value=[" + value + "]");
             }
         }
         finally {
